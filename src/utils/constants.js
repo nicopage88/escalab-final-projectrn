4e924a3046b809614b1606626ebdfb0cb60fdacb
NicoPage <nicolas.gonzalez.araneda@ciisa.cl>
2021-05-17 14:05:12 -0400

    primer commit

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const API_HOST = 'https://api.themoviedb.org/3';
+export const API_KEY = '6aa8ef6fea331056b63691a112e3c1b2';
+export const LANG = 'es-ES';
+export const BASE_PATH_IMG = 'https://image.tmdb.org/t/p';
